ff51b9b7a6a3141859f9d527d112576432d77647
d10sfan <d10sfan@gmail.com>
2020-05-22 20:42:49 -0500

    Script to find engine name from commit log

---
+++ README.md
@@ -0,0 +1,8 @@
+# action-build-get-engine-name
+
+GitHub Action to get engine name when commit has been created with engines change
+
+## Building
+
+* Make any changes to the index.js
+* Once done, run the following command `node_modules/@zeit/ncc/dist/ncc/cli.js build index.js` and create a tag
---
+++ index.js
@@ -0,0 +1,56 @@
+const core = require('@actions/core');
+const { context, GitHub } = require('@actions/github');
+
+console.log('Starting.');
+
+async function run() {
+    try {
+        const github = new GitHub(core.getInput('token'));
+        const commits = context.payload.commits.filter(c => c.distinct);
+        const repository = context.payload.repository;
+        const organization = repository.organization;
+        const owner = organization || repository.owner;
+        
+        let engineName;
+
+        for(let i = 0; i < commits.length; i++) {
+            const args = {
+                owner: owner,
+                repo: repository.name,
+                ref: commits[i].id
+            };
+            
+            const ret = await github.repos.getCommit(args);
+            
+
+            if(ret && ret.data && ret.data.files) {
+                for(let y = 0; y < ret.data.files.length; y++) {
+                    const filename = ret.data.files[y].filename;
+                    
+                    const filePath = filename.split('/');
+                    if(filePath[0] === 'engines') {
+                       engineName = filePath[1];
+                    }
+                    
+                    break;
+                }
+            }
+            
+            if(engineName) {
+                break;
+            }
+        }
+        
+        if(engineName) {
+            console.log(`Found Engine Name: ${engineName}`);
+            core.setOutput('engine', engineName);
+        } else {
+            core.setFailed('Failed to find engine name');
+        }
+    }
+    catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+run();
