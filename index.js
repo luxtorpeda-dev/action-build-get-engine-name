cae703c7e9cb6a4170287a76532267e114d4022f
d10sfan <sales@circuitcube.com>
2020-08-08 00:16:06 -0500

    Container name (#3)
    * Add support for custom container
    
    * fix
    
    * Change data
    
    Co-authored-by: d10sfan <d10sfan@gmail.com>

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ async function run() {
             console.log(`Found Engine Name: ${engineName}`);
             core.setOutput('engine', engineName);
             
-            let container = 'steamruntime';
+            let container = 'luxtorpedadev/runtime:7';
             
             const envFileStr = await fs.readFile(path.join('engines', engineName, 'env.sh'), 'utf-8');
             const envFileArr = envFileStr.split(/\r?\n/);
