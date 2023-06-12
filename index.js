4fc19741f53e0872a68fe7601eace916b969a563
d10sfan <d10sfan@gmail.com>
2023-06-11 20:34:54 -0500

    Update sniper runtime version

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ async function run() {
             console.log(`Found Engine Name: ${engineName}`);
             core.setOutput('engine', engineName);
             
-            let container = 'registry.gitlab.steamos.cloud/steamrt/sniper/sdk:0.20230405.47175';
+            let container = 'registry.gitlab.steamos.cloud/steamrt/sniper/sdk:0.20230509.49493';
             
             const envFileStr = await fs.readFile(path.join('engines', engineName, 'env.sh'), 'utf-8');
             const envFileArr = envFileStr.split(/\r?\n/);
