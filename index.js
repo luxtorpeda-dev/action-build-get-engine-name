4e341d823177c5421c2eefa61706397af1988c39
d10sfan <d10sfan@gmail.com>
2025-03-14 21:14:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ async function run() {
             console.log(`Found Engine Name: ${engineName}`);
             core.setOutput('engine', engineName);
             
-            let container = 'registry.gitlab.steamos.cloud/steamrt/sniper/sdk:0.20250306.120299';
+            let container = 'registry.gitlab.steamos.cloud/steamrt/sniper/sdk:3.0.20250210.116596';
             
             console.log(`Found container name: ${container}`);
             core.setOutput('container', container);
