85c3327ce5467f7122a7821c70561086a6ce2dea
GitHub Action <action@github.com>
2025-11-20 02:32:07 +0000

    Update dist

--- dist/index.js
+++ dist/index.js
@@ -31885,7 +31885,7 @@ async function run() {
             console.log(`Detected engines: ${engineList.join(', ')}`);
             core.setOutput('engines', JSON.stringify(engineList));
 
-            const container = 'registry.gitlab.steamos.cloud/steamrt/sniper/sdk:3.0.20250929.168600';
+            const container = 'registry.gitlab.steamos.cloud/steamrt/steamrt4/sdk:4.0.20251117.183306';
             console.log(`Using container: ${container}`);
             core.setOutput('container', container);
         } else {
